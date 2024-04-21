07a70b102e9efbda5c695d7b2db724559005c9ee
Hog <64306705+W3rtJack@users.noreply.github.com>
2024-04-21 19:37:02 +0100

    Add files via upload

---
+++ Custom Hog's Enchant Example/scripts/main.js
@@ -0,0 +1,1 @@
+import {} from "smashing.js"
---
+++ Custom Hog's Enchant Example/scripts/smashing.js
@@ -0,0 +1,32 @@
+import * as mc from "@minecraft/server"
+import { registerEnchant, slots } from "enchantment.js";
+
+// Declaring Enchant
+const ench = registerEnchant("smashing","Smashing",3,slots.sword);
+
+// Event start
+mc.world.afterEvents.entityHitEntity.subscribe(a =>{
+    const hit = a.hitEntity;
+    const attacker = a.damagingEntity;
+
+    if (attacker.typeId == "minecraft:player"){
+        // Getting items
+        const equipment = attacker.getComponent("equippable");
+        const item = equipment.getEquipment("Mainhand");
+
+        if (!item) return
+
+        // Setting values
+        const {valid, level} = ench.getItem(item);
+
+        // If the enchantment is on
+        if (valid){
+            // Running valid code
+            const vel = attacker.getVelocity()
+
+            if (!attacker.isOnGround && vel.y < -0.1 && !attacker.isSneaking){
+                attacker.applyKnockback(vel.x,vel.z,0,level*0.4+0.3)
+            }
+        }
+    }
+})
